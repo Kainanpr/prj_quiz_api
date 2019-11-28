96d4c52bbc231ae097d958bb205ae885a09f6cb5
KainanPR <kainanpr95@gmail.com>
2019-11-27 21:18:39 -0300

    Clearing tables

--- src/main/resources/db/migration/V0.1_2__Create_user_table.sql
+++ src/main/resources/db/migration/V0.1_2__Create_user_table.sql
@@ -9,8 +9,4 @@ create table user (
 );
 
 insert into user (id, name, email, password, is_admin) values
-(1, 'Carlos Alves', 'carlos.alves@gmail.com', '$2a$10$RZM.5.LvWpP2kUYLFYB45.wIQ3mSxNG.g8yCE0Wup.Ta7tUqw4iPu', 1),
-(2, 'Tiago Santos', 'tiago.santos@gmail.com', '$2a$10$F7uqplE2EId7UdQ5w4XrpOxrzv1Ppi2DCqsN.K299w5Nbs7D9/tbi', 0),
-(3, 'Rafael Gomes', 'rafael.gomes@gmail.com', '$2a$10$SY3YuuoE04Qh9.3MThEBjO60aXF2YcfxDpm1c74oFnkzRFlTEBjZa', 0),
-(4, 'João Andrade', 'joao.andrade@gmail.com', '$2a$10$sP9eVlRBVLvmqlkPGq6wt.2U2HFqJWm2V3HzCeC/id.RFv/c2Oc06', 0),
-(5, 'Igor Neves', 'igor.neves@gmail.com', '$2a$10$5NsPlFZ5PPmZi4eEBLoJg.rwhb1.G0wb2F86xgUw1Yd.bD6upDWo2', 0);
+(1, 'Admin', 'admin', '$2a$10$bnSykDJ3YBEb6Y0B6dbez.jCXLOVGhc6cBV5fXRvtOovSkI/3qTQO', 1),
--- src/main/resources/db/migration/V0.1_5__Create_study_table.sql
+++ src/main/resources/db/migration/V0.1_5__Create_study_table.sql
@@ -8,15 +8,3 @@ create table study (
   constraint study_fk_content foreign key (content_id) references content (id),
   constraint study_fk_level foreign key (level_id) references level (id)
 );
-
-insert into study (id, word, translation, content_id, level_id) values
-(1, 'Dent', 'Mossa', 2, 1),
-(2, 'Nicks', 'Cortes', 2, 1),
-(3, 'Scratches', 'Arranhões', 2, 1),
-(4, 'Cracks', 'Rachaduras', 2, 1),
-(5, 'Holes', 'Perfurações', 2, 1),
-(6, 'Abrasion', 'Abrasão', 2, 1),
-(7, 'Gouge', 'Ranhura', 2, 1),
-(8, 'Corrosion', 'Corrosão', 2, 1),
-(9, 'Delamination', 'Delaminação', 2, 1),
-(10, 'Disbond', 'Decolar', 2, 1);
